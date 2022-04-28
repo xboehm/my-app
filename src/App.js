6008418d39648489e7cba4d5edffe7356b468d60
boehmfel <boehmfel@hs-pforzheim.de>
2022-04-28 21:35:05 +0200

    appkl

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Say cheese! Flash.
           Coffee is great! Indeed.
+          BBQ
         </p>
         <a
           className="App-link"
