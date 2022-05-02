9eb75d7c67f5c1103794fac3ab89c48906e7969b
boehmfel <boehmfel@hs-pforzheim.de>
2022-05-02 22:02:58 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Say cheese! Flash.
           Coffee is great! Indeed.
-          BBQab
+          BBQabc
         </p>
         <a
           className="App-link"
