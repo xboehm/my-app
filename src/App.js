e94ace9a46a1cbfaa0d52cbd091f95a158a1a2de
boehmfel <boehmfel@hs-pforzheim.de>
2022-05-18 11:50:47 +0200

    added new feature to App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
 
           Edit <code>src/App.js</code> and save to reload.
-          Say cheese! Flash. 6 
+          Say cheese! Flash. 7 
           Coffee is great! Indeed.
           BBQabcd
           v1.1.2
