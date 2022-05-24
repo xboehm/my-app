0c97d549e1224ecb2edf0e0244a4769692a831ca
boehmfel <boehmfel@hs-pforzheim.de>
2022-05-24 11:08:47 +0200

    new update to 1.1.6

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Say cheese! Flash. 8 
           Coffee is great! Indeed. <br></br>
-          v1.1.5
+          v1.1.6
         </p>
         <a
           className="App-link"
