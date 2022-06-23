3e71c5c0097e4b779812e59eb933aed17867cbd1
boehmfel <boehmfel@hs-pforzheim.de>
2022-06-23 17:18:36 +0200

    added: new version number

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
           Say cheese! a
           Coffee is great! Indeed. <br></br>
-          v1.1.15<br></br>
+          v1.1.17<br></br>
           {msg} {pic}
         </p>
         <a
